3e3a7d20512532f4991864424ed961e57a3646dc
laura saldarriaga giraldo <laurasaldarriaga74@gmail.com>
2026-05-11 19:43:09 -0500

    nuevo proyecto

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-048b44d7-d3fa-423d-8837-f3904668847e.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
